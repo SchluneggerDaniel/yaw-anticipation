5c8a769a6cd9084c91503115772e70a6c24c0cb2
Andrew Ellis <a.w.ellis@gmail.com>
2016-09-20 16:16:24 +0200

    added R code used to create dataset

---
+++ R/create-dataset.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(readr)
+
+yaw <- read_csv(file = "data/dataDaniel.csv", col_types = cols(X1 = col_skip()))
+
+names(yaw) <- c("trialno", "rt", "response", "intensity", "accuracy", "id")
+
+yaw <- within(yaw, {
+    response <- ifelse(response == 1, 0, 1)
+    id <- as.factor(id)
+})
+
+write_csv(x = yaw, path = "data/yaw-anticipation.csv")
+
