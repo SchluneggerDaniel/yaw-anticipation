aac9d33f219722434d360c3a7f01bb36fa4c5fe8
Andrew Ellis <a.w.ellis@gmail.com>
2016-09-20 16:28:07 +0200

    added log rt

--- R/create-dataset.R
+++ R/create-dataset.R
@@ -6,8 +6,10 @@ yaw <- read_csv(file = "data/dataDaniel.csv", col_types = cols(X1 = col_skip()))
 names(yaw) <- c("trialno", "rt", "response", "intensity", "accuracy", "id")
 
 yaw <- within(yaw, {
+    # recode so that left responses are coded as 1, right responses are 0
     response <- ifelse(response == 1, 0, 1)
     id <- as.factor(id)
+    logrt <- log(rt)
 })
 
 # write_csv(x = yaw, path = "data/yaw-anticipation.csv")
